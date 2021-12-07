fe165f71087c28b0d1cda6b6bc63d4102edaab0c
Eike Rogall <eike.rogall@gmail.com>
2021-12-07 10:44:07 +0100

    Day7

--- 2021/day7.py
+++ 2021/day7.py
@@ -14,11 +14,9 @@ def calculate_fuel_1(crab):
     return sum(crab_fuel)
 
 def calculate_fuel_2(crab):
-    horizontal_pos_mean = 478 #round(mean(crab))
-    horizontal_pos_median = round(median(crab))
+    horizontal_pos_mean = int(mean(crab))
     crab_fuel_mean = [int(abs(x - horizontal_pos_mean)) for x in crab]
-    crab_fuel_median = [int(abs(x - horizontal_pos_median)) for x in crab]
-    return f"Mean: {sum([(x * (x+1)/2) for x in crab_fuel_mean])}\nMedian: {sum([(x * (x+1)/2) for x in crab_fuel_median])}"
+    return sum([(x * (x+1)/2) for x in crab_fuel_mean])
 
 
 
