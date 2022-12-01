e8450133fe2bc87eb9fc3e9f22163ddff62aea30
Eike Rogall <eikerogall@MacBook-Pro-von-Eike.fritz.box>
2022-12-01 09:36:21 +0100

    Day1 2022

---
+++ 2022/Day1.py
@@ -0,0 +1,23 @@
+with open('2022/input_files/day1.txt') as f:
+    lines = f.read()
+
+def get_data(data):    
+    data = data.split('\n')
+    #data.remove('')
+    return data
+
+data = get_data(lines)
+temp_lst = []
+result = []
+data.append('')
+
+
+for idx, i in enumerate(data):
+    if i != '':
+        temp_lst.append(int(i))
+    elif i == '':
+        result.append(sum(temp_lst))
+        temp_lst = []
+
+result = sorted(result, reverse=True)
+print(sum(result[:3]))
