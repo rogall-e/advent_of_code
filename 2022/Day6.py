1841c75e4daa1da8bfd84217a8fa6ed622e0244e
Eike Rogall <eikerogall@MBP-von-Eike.fritz.box>
2022-12-06 09:40:37 +0100

    Day6 2022

--- 2022/Day6.py
+++ 2022/Day6.py
@@ -3,32 +3,20 @@ with open('2022/input_files/day6.txt') as f:
 
 
 
-def part_1(lines):
+def solution(lines, codon=4):
     reading_frame = []
     solution = 0
     for idx,item in enumerate(lines):
         reading_frame.append(item)
-        if len(reading_frame) == 4:
-            if len(set(reading_frame)) == 4:
+        if len(reading_frame) == codon:
+            if len(set(reading_frame)) == codon:
                 solution = idx +1
                 break
             else:
                 reading_frame.pop(0)
     return solution      
 
-def part_2(lines):
-    reading_frame = []
-    solution = 0
-    for idx,item in enumerate(lines):
-        reading_frame.append(item)
-        if len(reading_frame) == 14:
-            if len(set(reading_frame)) == 14:
-                solution = idx +1
-                break
-            else: 
-                reading_frame.pop(0)
-    return solution  
 
-print(part_1(lines))
-print(part_2(lines))
+print(solution(lines))
+print(solution(lines, 14))
     
